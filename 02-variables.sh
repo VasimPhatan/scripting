5f73897126b6c5da6969e890d1c7f7a255143dad
vasim <vasimphatan26@gmail.com>
2025-11-15 17:36:08 +0530

    seventhhhhg

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,11 +4,23 @@
 
 
 echo "plese enter username"
-read USERNAME #username  #username will be stored in this USERNAME variable
+read USERNAME              #username will be stored in this USERNAME variable
+
+echo "please enter password"
+read -s PASSWORD             #password  will be stored in this USERNAME variable , but while entering the passsword it should not print on terminal so we ill use -s option.
+
+
+
+
 
 
 #printing the username
 echo "user is $USERNAME"
+
+
+#printing the password
+echo "passowerd entered is $PASSWORD"
+
 
 
 
