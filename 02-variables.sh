61ef4637721175fb1c9ae1f13b01daee6c63c0c0
vasim <vasimphatan26@gmail.com>
2025-11-15 17:24:19 +0530

    seventhhhg

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,31 +1,9 @@
 #!/bin/bash
 
-#taking inputs before executing  the script
+#providing the data to the variable during executing the script we will use read
 
 
-a=$1
-b=$2
+echo "plese enter username"
 
-SCRIPT_NAME=$0
-
-TOTAL_VARIABLES_PASSED=$*
-TOTAL_VARIABLES_PASSED_1=$@
-NUMBER_OF_VARIABLES_PASSED=$#
-
-
-
-
-echo "vaulue of a is $a"
-
-echo "value of b is $b"
-
-
-echo "to print the total variables passed $TOTAL_VARIABLES_PASSED"
-
-echo "to print the number of variables passed $NUMBER_OF_VARIABLES_PASSED"
-
-echo "to print the script name $SCRIPT_NAME"
-
-
-
+read USERNAME #username  #username will be stored in this USERNAME variable
 
