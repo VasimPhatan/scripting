924e5e2813e58d1fe0ed269b23836e571f668084
vasim <vasimphatan26@gmail.com>
2025-11-15 16:30:02 +0530

    fifth

--- 01-print.sh
+++ 01-print.sh
@@ -4,12 +4,15 @@
 echo " hello world"
 
 # if we want to add colours to the output"
-
 echo -e " \e[32m my name is vasim \e[0m"
 
 #if we want to give tab space b/w  data 
-
 echo  -e "my name is\tvasim"
+
+#if we want to print the vasim in the next line
+echo -e "my name is \n vasim"
+
+
 
 
 
