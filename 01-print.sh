f5bd21103dac9eb62b26bf0c573b9f9b453385a9
vasim <vasimphatan26@gmail.com>
2025-11-29 19:43:57 +0530

    jbdfjsbddddgddsfdfddd

--- 01-print.sh
+++ 01-print.sh
@@ -3,7 +3,7 @@
 # we will implement colours
 # we will implement validation.
 # we will check if apache and nginx services are alreday installed or not
-
+#we will check if packages already exists skip the installtion if not install.
 
 
 DATE=$(date +%F) 
@@ -15,18 +15,26 @@ G="\e[32m"
 N="\e[0m"
 
 
+VALIDATE () {
+    if [ $1 -ne 0 ]; then  
+        echo -e "installation of $2...........$R failure $N"
+        exit 1
+    else 
+        echo -e "installation of $2...........$G success $N"
+    fi
+}
 
 
 for i in $@
 do
-    yum install $i -y  &>>$LOG_FILE
-    if [ $? -ne 0 ]; then
-        echo -e "installation of $i......... $R failure $N"
+yum list installed | grep $i
+    if [ $? -ne 0 ]; then   
+        echo "$i is alreday installed no need to install"
         exit 1
     else
-        echo  -e "installation of $i........ $G success $N"
-    fi   
-
+    echo "$i is not present installing the packages"
+    yum install $i -y  &>>$LOG_FILE
+    VALIDATE $? $i
 done
 
 
