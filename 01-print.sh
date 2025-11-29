ecdaca974ad8fd67ba3e6e77fe99f9b01aef2174
vasim <vasimphatan26@gmail.com>
2025-11-29 19:29:19 +0530

    jbdfjsbddddgddsfdfddd

--- 01-print.sh
+++ 01-print.sh
@@ -17,10 +17,10 @@ N="\e[0m"
 
 VALIDATE () {
     if [ $1 -ne 0 ]; then  
-        echo -e "$2...........$R failure $N"
+        echo -e "installation of $2...........$R failure $N"
         exit 1
     else 
-        echo -e "$2...........$G success $N"
+        echo -e "installation of $2...........$G success $N"
     fi
 }
 
