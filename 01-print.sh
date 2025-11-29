5f899eab4df21b9e217aea4e27265830e0a16d08
vasim <vasimphatan26@gmail.com>
2025-11-29 19:22:36 +0530

    jbdfjsbddddgddsfdfddd

--- 01-print.sh
+++ 01-print.sh
@@ -10,16 +10,19 @@ DATE=$(date +%F)
 LOGDIR=/tmp
 SCRIPT_NAME=$0
 LOG_FILE=$LOGDIR/$SCRIPT_NAME-$DATE.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 
 for i in $@
 do
     yum install $i -y  &>>$LOG_FILE
     if [ $? -ne 0 ]; then
-        echo "installation of $i.........failure"
+        echo -e "installation of $i......... $R failure $N"
         exit 1
     else
-        echo "installation of $i........success"
+        echo "installation of $i........ $G success $N"
     fi   
 
 done
