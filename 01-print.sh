08936f9d380198673e866f28083764600b010a35
vasim <vasimphatan26@gmail.com>
2025-11-29 18:46:22 +0530

    jbdfjsbddddgdd

--- 01-print.sh
+++ 01-print.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#if we know the list of items we will go with for loop/ 
+#if we know the list of items we will go with for loop 
 
-# loops with list of numbers.
+# loops with range of numbers.
 
 example: for loop to print 5 numbers
 
-for i in 1 2 3 4 5 
+for i in {1....5}
 do
     echo "$i"
 done
