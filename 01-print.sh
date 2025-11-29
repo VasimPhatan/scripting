bbdce237bef2e6712f1e4a55d13a4e89f666b122
vasim <vasimphatan26@gmail.com>
2025-11-29 18:41:42 +0530

    jbdfjsbddddgdd

--- 01-print.sh
+++ 01-print.sh
@@ -1,41 +1,15 @@
 #!/bin/bash
 
-#now we will install httpd nginx services abnd we will use functions for validation. 
+#if we know the list of items we will go with for loop/ 
 
-DATE=$(date +%F)
-ID=$(id -u)
-SCRIPT_NAME=$0
-LOGDIR=/tmp/$SCRIPT_NAME-$DATE.log
+# loops with list of numbers.
 
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-VALIDATE () {
-    if [ $1 -ne 0 ]; then 
-      echo -e "$2.......... $R failure $N"
-      exit 1
-    else 
-       echo -e "$2........... $G sucecss $N"
-    fi
-}
+example: for loop to print 5 numbers
 
-
-
-if [ $ID -ne 0 ] ; then
-
-    echo  -e "you are not root user ...... $R you cannot continue the script $N"
-    exit 1
-else
-    echo -e "you are root user........ $G you can continue the script $N"
-fi
-
-yum install httpddd -y  &>>$LOGDIR
-VALIDATE $? "installtion of httpd"
-
-
-
-yum install nginx -y &>>$LOGDIR
-VALIDATE $? "installtion of nginx"
+for i in 1 2 3 4 5 
+do
+    echo "$i"
+done
 
 
 
