ee633cbfde96d97f41fd1442cc8b35384eb3b851
vasim <vasimphatan26@gmail.com>
2025-11-30 14:48:03 +0530

    jhbsdadjbk

--- 01-print.sh
+++ 01-print.sh
@@ -10,10 +10,9 @@
 DATE=$(date +%F-%H-%M-%s)
 APP_LOG_DIRECTORY=/home/centos/app_logs
 SCRIPT_NAME=$0
-LOG_FILE=$(/tmp/$SCRIPT_NAME-$APP_LOG_DIRECTORY-$DATE.log)
+LOG_FILE=$SCRIPT_NAME-$APP_LOG_DIRECTORY-$DATE.log
 
-
-echo "$LOG_FILE" > $LOG_FILE
+echo "$LOG_FILE"  &>>$LOG_FILE
 
 
 
